b1106f08b1c0102c1198022c32431dde52bbc742
norrisrahman <norisdr8@gmail.com>
2021-07-08 21:36:48 +0700

    Main

---
+++ components/elements/content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-table striped hover :items="tabel"></b-table>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    modules: ['@nuxtjs/axios'],
+    data() {
+      return {
+        tabel: []
+      }
+    },
+    methods : {
+      async getMahasiswa() {
+        const URL = "http://localhost:8080/api/mahasiswa/";
+        await this.$axios.$get(URL)
+          .then(res => {
+            console.log(res.data)
+          })
+      }
+    },
+    mounted() {
+      this.getMahasiswa();
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wrapper">
+    <header-app></header-app>
+    <div id="content-wrapper">
+      <div class="content-title"> <h1>Data Mahasiswa 2021</h1></div>
+      <content-app></content-app>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '../components/elements/header'
+import Content from '../components/elements/content'
+export default {
+  components : {
+    'header-app': Header,
+    'content-app': Content,
+  }
+}
+</script>
